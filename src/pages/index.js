7848dcfcf557e5f1b85fc1fd0904173cad948d5b
fdrive422 <fafrago@gmail.com>
2022-11-29 19:11:45 -0800

    productsFeed responsive page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import Banner from '../components/Banner';
 import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <div className="bg-gray-100">
       <Head>
@@ -18,8 +18,21 @@ export default function Home() {
         <Banner />
 
         {/* Product Feed */}
-        <ProductFeed />
+        <ProductFeed products={products} />
+
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products,
+    },
+  };
 }

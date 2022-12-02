9c9f7a59d21bd2436da5b47d9cb3f7015a7fc20d
fdrive422 <fafrago@gmail.com>
2022-12-01 19:27:42 -0800

    nextauth.js update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,18 +1,16 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-// import Providers from "next-auth/providers"
-// import GithubProvider from "next-auth/providers/github"
 
-export const authOptions = ({
+// export const authOptions = ({
+export default NextAuth({
 	// Configure one or more authentication providers
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_ID,
 			clientSecret: process.env.GOOGLE_SECRET,
-			nextAuth: process.env.NEXTAUTH_SECRET,
 		}),
 		// ...add more providers here
 	],
 });
 
-export default NextAuth(authOptions)
+// export default NextAuth(authOptions)
